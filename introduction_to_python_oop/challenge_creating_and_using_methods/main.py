c951308962045e7f08053cb51a14ae14adff38c7
zeino <>
2026-02-26 18:15:35 +0000

    Challenge: Creating and Using Methods

---
+++ introduction_to_python_oop/challenge_creating_and_using_methods/main.py
@@ -0,0 +1,14 @@
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+    def area(self):
+        return self.width*self.height
+
+    def perimeter(self):
+        return 2*(self.width + self.height)
+
+
+rect = Rectangle(4, 5)
+print(rect.area())
+print(rect.perimeter())
